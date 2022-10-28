3b9b575938a55c72818ef37ef25555dcb14ce663
irisu01 <nekomarisakirisame@gmail.com>
2022-10-28 19:55:12 +0000

    add account details to navbar

--- src/components/subcomponents/NavbarAccount.tsx
+++ src/components/subcomponents/NavbarAccount.tsx
@@ -1,14 +1,24 @@
+import { createEffect, createSignal } from "solid-js";
 import { useGlobals } from "../../contexts/GlobalProvider";
+import { AccountDetails } from "../../structures/Account";
 
 export function NavbarAccount() {
     const { account } = useGlobals();
+
     return (
         <div class="navbar-header--right-account">
-            {account.loggedIn ? <div>
-                
-            </div> : <div class="navbar-header--right-account-buttons">  
-                <a href="/auth?type=login" class="navbar-header--right-account-buttons--button">login</a>
-                <a href="/auth?type=register" class="navbar-header--right-account-buttons--button">register</a>
+            {account.getSignal<boolean>("login") ? <div class="navbar-header--right-account-details">
+                <div class="navbar-header--right-account-details--username">
+                    hello, <span>{account.getSignal<AccountDetails>("account")?.username}</span>! <button class="navbar-header--right-account-details--logout">(logout)</button>
+                </div>
+
+                <div class="navbar-header--right-account-buttons">
+                    <a href="#" class="navbar-header--right-account-buttons--button">profile <span style={{ color: "var(--grayscale-5)", "font-size": "0.75em"}}>(unfinished)</span></a>
+                    <a href="#" class="navbar-header--right-account-buttons--button">settings <span style={{ color: "var(--grayscale-5)", "font-size": "0.75em"}}>(unfinished)</span></a>
+                </div>
+            </div> : <div class="navbar-header--right-account-buttons">
+                <a href="/login" class="navbar-header--right-account-buttons--button">login</a>
+                <a href="/register" class="navbar-header--right-account-buttons--button">register</a>
             </div>}
         </div>
     )
--- src/structures/Account.ts
+++ src/structures/Account.ts
@@ -1,11 +1,105 @@
+import { Accessor, createSignal, Setter } from "solid-js";
 import { RestClient } from "../util/RestClient";
+
+export type AccountSignals = "login" | "account";
+export interface AccountDetails {
+    username?: string;
+}
 
 export class Account {
     public rest: RestClient;
-    public loggedIn: boolean;
+
+    // signals
+    private loginSignal: Accessor<boolean>;
+    private _setLoginSignal: Setter<boolean>;
+    public accountSignal: Accessor<AccountDetails | undefined>;
+    private _setAccountSignal: Setter<AccountDetails | undefined>;
 
     constructor() {
         this.rest = new RestClient(this);
-        this.loggedIn = false;
+
+        // check for a session
+        let token = localStorage.getItem("session");
+        const [ loginSignal, setLoginSignal ] = createSignal(false);
+        const [ accountSignal, setAccountSignal ] = createSignal<AccountDetails | undefined>();
+
+        this.loginSignal = loginSignal;
+        this._setLoginSignal = setLoginSignal;
+        this.accountSignal = accountSignal;
+        this._setAccountSignal = setAccountSignal;
+
+        if (token) {
+            this.validateSession(token);
+        }
+    }
+
+    // SIGNAL FUNCTIONS
+    public getSignal<T>(signal: AccountSignals): T | undefined {
+        const signalName = `${signal}Signal` as keyof Account;
+        if (this[signalName]) {
+            let getter = this[signalName] as Accessor<T>;
+
+            return getter();
+        }
+
+        return undefined;
+    }
+
+    public setSignal<T>(signal: AccountSignals, value: T): Account {
+        const signalName = `_set${signal.charAt(0).toUpperCase()}${signal.slice(1)}Signal` as keyof Account;
+        if (this[signalName]) {
+            let setter = this[signalName] as Setter<T>;
+
+            setter((_prev) => value);
+        }
+
+        return this;
+    }
+
+    // AUTH FUNCTIONS
+    public validateSession(token: string) {
+        this.rest.request("https://ncx.irisu.us/api/auth/validate", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                token: token
+            })
+        }).then((res) => {
+            if (res.data.valid) {
+                this.setSignal("login", true);
+                this.setSignal("account", res.data.account);
+            } else {
+                // delete token from local storage
+                localStorage.removeItem("session");
+                this.setSignal("login", false);
+            }
+        });
+    }
+
+    public login(username: string, password: string) {
+        return this.rest.request("https://ncx.irisu.us/api/auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        })
+    }
+
+    public register(username: string) {
+        return this.rest.request("https://ncx.irisu.us/api/auth/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: username
+            })
+        })
     }
 }
