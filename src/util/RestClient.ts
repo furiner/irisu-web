50a23b89c40da4038380ac6ef4896437a621d76a
irisu01 <nekomarisakirisame@gmail.com>
2022-10-28 19:39:10 +0000

    make request return a promise

--- src/util/RestClient.ts
+++ src/util/RestClient.ts
@@ -11,27 +11,29 @@ export class RestClient {
     /**
      * Wraps around the Fetch API to send a request.
     */
-    public async request(url: string, request: RequestInit): Promise<any> {
-        const response = await fetch(url, request);
+    public request(url: string, request: RequestInit): Promise<any> {
+        return new Promise(async(resolve, reject) => {
+            const response = await fetch(url, request)
 
-        // Handle status codes
-        if (response.status == 200) {
-            if (response.headers.get("Content-Type")?.startsWith("application/json")) {
-                return await response.json();
+            // Handle status codes
+            if (response.status == 200) {
+                if (response.headers.get("Content-Type")?.startsWith("application/json")) {
+                    resolve(await response.json());
+                } else {
+                    resolve(await response.text());
+                }
+            } else if (response.status >= 500) {
+                reject(new APIResponseError(response.status, "A potential internal error has occured.", await response.text()));
             } else {
-                return await response.text();
+                let data;
+                if (response.headers.get("Content-Type")?.startsWith("application/json")) {
+                    data = await response.json();
+                } else {
+                    data = await response.text();
+                }
+    
+                reject(new APIResponseError(response.status, data?.message ?? data));
             }
-        } else if (response.status >= 500) {
-            throw new APIResponseError(response.status, "A potential internal error has occured.", await response.text());
-        } else {
-            let data;
-            if (response.headers.get("Content-Type")?.startsWith("application/json")) {
-                data = await response.json();
-            } else {
-                data = await response.text();
-            }
-
-            throw new APIResponseError(response.status, data?.message ?? data);
-        }
+        });
     }
 }
