8745a9100ee93f5b1c3c5868b9aca1ff6ce4dec8
irisu01 <nekomarisakirisame@gmail.com>
2022-10-28 00:30:32 +0000

    change login redirects to the actual page

--- src/components/subcomponents/NavbarAccount.tsx
+++ src/components/subcomponents/NavbarAccount.tsx
@@ -7,8 +7,8 @@ export function NavbarAccount() {
             {account.loggedIn ? <div>
                 
             </div> : <div class="navbar-header--right-account-buttons">  
-                <a href="/a" class="navbar-header--right-account-buttons--button">login</a>
-                <a href="#" class="navbar-header--right-account-buttons--button">register</a>
+                <a href="/auth?type=login" class="navbar-header--right-account-buttons--button">login</a>
+                <a href="/auth?type=register" class="navbar-header--right-account-buttons--button">register</a>
             </div>}
         </div>
     )
